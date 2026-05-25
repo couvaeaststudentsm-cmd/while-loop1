b9b4d6a1fc20656e68894d1a801b18c4d7902924
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-05-25 18:17:39 -0400

    Add files via upload

---
+++ program37.py
@@ -0,0 +1,6 @@
+bill_amount = float(input("Enter the total bill amount:"))
+paid_amount = float(input("Enter the paid amount"))
+
+due_amount = bill_amount - paid_amount
+
+print("Due Amount =", due_amount)
