c353e14c7a30a3cc2f36c821566c647e60c83af8
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-05-30 19:36:35 -0400

    Update program42.py

--- program42.py
+++ program42.py
@@ -1,1 +1,15 @@
+try:
+    age = int(input("Enter your age: "))
 
+    if age < 0:
+        print("Invalid age!")
+    else:
+        print("Valid age.")
+
+        if age % 2 == 0:
+            print("Age is even.")
+        else:
+            print("Age is odd.")
+
+except ValueError:
+    print("Error! Please enter a valid number.")
