3ba22e1c90dc0492b0c19e3e07589775ac490f52
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-06-05 19:38:56 -0400

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,7 @@
+import math
+
+angle = int(input("Enter angle: "))
+
+print("Sin =", math.sin(math.radians(angle)))
+print("Cos =", math.cos(math.radians(angle)))
+print("Tan =", math.tan(math.radians(angle)))
