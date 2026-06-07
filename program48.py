876566834ee1ccb6bbac6663243c5eefdb5a7735
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-06-06 20:58:45 -0400

    Add files via upload

---
+++ program48.py
@@ -0,0 +1,12 @@
+L =[4,5,1,2,9,7,10,8]
+print("orginal list:",L)
+count = 0
+for i in L:
+    count +=i
+avg = count/len(L)
+print("sum =:",count)
+print("Average =",avg)
+L.sort()
+
+print("Smallest element is:",L[0])
+print("Largest element is:",L[-1])
