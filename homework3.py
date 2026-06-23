7d4a2a86506636964482a1cb95e043dad0d27fcd
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-06-23 17:37:42 -0400

    Create homework3.py

---
+++ homework3.py
@@ -0,0 +1,9 @@
+
+
+test_dict = {'Codingal': 2, 'is': 2, 'best': 1, 'for': 2, 'Coding': 1}
+
+value = 2
+
+frequency = list(test_dict.values()).count(value)
+
+print("Frequency of", value, "is:", frequency)
