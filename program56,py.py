d5d5058e8f62b2ef9322aea843e54d8637bac918
couvaeaststudentsm-cmd <couvaeaststudentsm@gmail.com>
2026-07-03 21:06:06 -0400

    Add files via upload

---
+++ program56,py.py
@@ -0,0 +1,11 @@
+fruits = ["apple","banana","cherry"]
+print(fruits)
+
+x = (10,20,30)
+print(x)
+
+grades = {"Shiva":95,"Bob":88}
+print(grades)
+
+numbers = {1,2,3,3,3,3}
+print(numbers)
---
+++ program57.py
@@ -0,0 +1,33 @@
+list1 = [10,20,30]
+list2 = [40,50,60]
+
+print("Concatenation:")
+print(list1+ list2)
+
+print("\nRepetiton:")
+print(list1*2)
+
+print("\nMembership(in):")
+print(20 in list1)
+
+print("\nMembership(not in):")
+print(100 in list1)
+
+print("\nIndexing:")
+print("First element:",list1[0])
+print("last  element:",list1[-1])
+
+print('\nSlicing:')
+print(list1[0:2])
+
+a = [1,2,3]
+b = [1,2,3]
+c = [4,5,6]
+
+print("\nComparasion")
+print(a == b)
+print(a != b)
+print(a > b)
+print(a  <b)
+
+
